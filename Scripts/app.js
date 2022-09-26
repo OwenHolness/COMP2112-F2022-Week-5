ae87f822b1af27666aa78fe25e5b249861e48c06
Owen Holness <Owen.Holness@MyGeorgian.ca>
2022-09-26 08:57:46 -0400

    updated project files

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,48 +2,12 @@
 // IIFE -- Immediately Invoked Function Expression
 // AKA -- Self Executing Function
 (function () {
-    /**
-     * This function loads data Asynchronously from a URL.
-     *It calls the callback funtion when the data loading is complete
-     * @param {string} method
-     * @param {string} url
-     * @param {function} callback
-     */
-    function LoadData(method, url, callback) {
-        // Step 1 - Create an empty XHR object
-        let XHR = new XMLHttpRequest();
-        // Step 2 - Compose the Request
-        XHR.open(method, url);
-        // Step 3 - Send the request to the server
-        XHR.send();
-        // Step 4 - Setup an event listener
-        XHR.addEventListener("readystatechange", function () {
-            if (XHR.status == 200 && XHR.readyState == 4) {
-                callback(XHR.responseText);
-                // let contactDataSource = JSON.parse(XHR.responseText)
-                // console.log(contactDataSource.ContactList[2])
-            }
-        });
-    }
-    // First method of using functions
     function Start() {
         console.log("App Started!");
-        // LoadData("GET", "./Data/contacts.json", function(XHR){
-        //     console.log(XHR);
-        // });
         $.getJSON("./Data/contacts.json", function (DataSource) {
             console.log(DataSource.ContactList[2]);
         });
     }
-    // Second method of usi ng functions
-    // let Start = function()
-    // {
-    //     console.log("App Started!");
-    // }
-    // Third method of using functions
-    // let Start = () => {
-    //     console.log("App Started!");
-    // }
     window.addEventListener("load", Start);
 })();
 //# sourceMappingURL=app.js.map
